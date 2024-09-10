0a50902819c0aea66a50f4ea90c18698b046bfdd
songjimin <ssong503503@gmail.com>
2024-09-10 23:24:05 +0900

    feat: day02 과제 제출

---
+++ challenge/onebite-cinema/src/pages/search/index.tsx
@@ -0,0 +1,11 @@
+import { useRouter } from "next/router"
+
+export default function Page () {
+    const router = useRouter();
+
+    const { q } = router.query;
+
+    return (
+        <h1>검색 결과 : {q}</h1>
+    )
+}
