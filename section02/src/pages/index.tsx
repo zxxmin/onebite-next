cc46f9304175e478ec25578fce84a83533167b5c
songjimin <ssong503503@gmail.com>
2024-09-11 16:52:37 +0900

    네비게이팅 + 프리페칭 + API Routes + 스타일링

--- section02/src/pages/index.tsx
+++ section02/src/pages/index.tsx
@@ -1,6 +1,12 @@
+// CSS module
+import style from './index.module.css';
 
 export default function Home() {
   return (
-    <h1>인덱스</h1>
+    <>
+    <h1 style={{color: 'red'}}>인라인 스타일</h1>
+    <h1 className={style.h1}>CSS 모듈</h1>
+    <h2 className={style.h2}>H2</h2>
+    </>
   );
 }
