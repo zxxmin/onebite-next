80806851649c468a42168fe7ab6f442380152263
songjimin <ssong503503@gmail.com>
2024-09-24 11:00:49 +0900

    한입북스 UI 구현하기

--- section02/src/pages/index.tsx
+++ section02/src/pages/index.tsx
@@ -2,14 +2,25 @@
 import SearchableLayout from '@/components/searchable-layout';
 import style from './index.module.css';
 import { ReactNode } from 'react';
+import books from '@/mock/books.json'
+import BookItem from '@/components/book-item';
 
 export default function Home() {
   return (
-    <>
-    <h1 style={{color: 'red'}}>인라인 스타일</h1>
-    <h1 className={style.h1}>CSS 모듈</h1>
-    <h2 className={style.h2}>H2</h2>
-    </>
+    <div className={style.container}>
+      <section>
+        <h3>지금 추천하는 도서</h3>
+        {books.map((book) => (
+          <BookItem key={book.id} {...book} />
+        ))}
+      </section>
+      <section>
+        <h3>등록된 모든 도서</h3>
+        {books.map((book) => (
+          <BookItem key={book.id} {...book} />
+        ))}
+      </section>
+    </div>
   );
 }
 
--- section02/src/pages/search/index.tsx
+++ section02/src/pages/search/index.tsx
@@ -1,16 +1,15 @@
 import SearchableLayout from "@/components/searchable-layout";
 import { useRouter } from "next/router"
 import { ReactNode } from "react";
+import books from '@/mock/books.json'
+import BookItem from "@/components/book-item";
 
 export default function Page() {
-
-    const router = useRouter();
-
-    const { q } = router.query;
-
-    console.log(router)
-
-    return <h1>Search {q}</h1>
+    return <div>
+        {books.map((book) => (
+            <BookItem key={book.id} {...book} />
+        ))}
+    </div>
 }
 
 Page.getLayout = (page: ReactNode) => {
