d56340ecef7c9a16efd1531f90104555381c5e8e
songjimin <ssong503503@gmail.com>
2024-09-25 10:01:16 +0900

    사전 렌더링과 데이터 페칭 + SSR 1. 소개 및 실습 + SSR 2. 실습

--- section02/src/pages/book/[id].tsx
+++ section02/src/pages/book/[id].tsx
@@ -1,4 +1,6 @@
+import { GetServerSidePropsContext, InferGetServerSidePropsType } from 'next';
 import style from './[id].module.css'
+import fetchOneBooks from '@/lib/fetch-one-book';
 
 const mockData = {
     "id": 1,
@@ -10,7 +12,24 @@ const mockData = {
     "coverImgUrl": "https://shopping-phinf.pstatic.net/main_3888828/38888282618.20230913071643.jpg"
 }
 
-export default function Page () {
+export const getServerSideProps = async (context : GetServerSidePropsContext) => {
+    
+    const id = context.params!.id;
+    const book = await fetchOneBooks(Number(id))
+
+
+    return {
+        props: {
+            book,
+        },
+    }
+}
+
+export default function Page ({
+    book
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+    if(!book) return '문제가 발생했습니다. 다시 시도하세요.';
+
     const {
         id,
         title,
@@ -19,7 +38,7 @@ export default function Page () {
         author,
         publisher,
         coverImgUrl,
-    } = mockData
+    } = book;
 
     return <div className={style.container}>
         <div
