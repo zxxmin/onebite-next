cf14ed3e3f1efcea9db6002653914a9b19665a9c
songjimin <ssong503503@gmail.com>
2024-10-08 00:06:35 +0900

    feat: day12 과제 제출 - 데이터 페치 실습

--- challenge/one-cinema-app/src/app/(with-searchbar)/page.tsx
+++ challenge/one-cinema-app/src/app/(with-searchbar)/page.tsx
@@ -2,6 +2,45 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import movies from "@/mock/movies.json"
 import MovieItem from "@/components/movie-item";
+import { MovieData } from "@/types";
+
+async function AllMovies () {
+  // 등록된 모든 영화는 한번 불러온 후 데이터 변화가 없으므로 'force-cache' 설정
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/movie`,
+    { cache: 'force-cache' }
+  )
+  if(!res.ok) return <div>오류가 발생하였습니다!!!</div>
+
+  const allMovies:MovieData[] = await res.json();
+
+  return <ul className={styles.now_container}>
+    {allMovies.map((movie) => (
+      <li key={`now-${movie.id}`}>
+        <MovieItem {...movie} />
+      </li>
+    ))}
+  </ul>
+}
+
+async function BestMovies () {
+  // 새로고침이나 페이지 이동 시 계속된 랜덤했으면 좋겠어서 'no-store' 기본값 설정
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/movie/random`
+  )
+  if(!res.ok) return <div>오류가 발생하였습니다!!!</div>
+
+  const bestMovies:MovieData[] = await res.json();
+
+  return <ul className={styles.best_container}>
+    {bestMovies.slice(0, 3).map((movie) => (
+      <li key={`best-${movie.id}`}>
+        <MovieItem {...movie} />
+      </li>
+    ))}
+  </ul>
+}
+
 
 
 export default function Home() {
@@ -9,23 +48,11 @@ export default function Home() {
     <div className={styles.container}>
       <section>
         <h3>지금 가장 추천하는 영화</h3>
-        <ul className={styles.best_container}>
-          {movies.slice(0, 3).map((movie) => (
-            <li key={`best-${movie.id}`}>
-              <MovieItem {...movie} />
-            </li>
-          ))}
-        </ul>
+        <BestMovies />
       </section>
       <section>
         <h3>등록된 모든 영화</h3>
-        <ul className={styles.now_container}>
-          {movies.map((movie) => (
-            <li key={`now-${movie.id}`}>
-              <MovieItem {...movie} />
-            </li>
-          ))}
-        </ul>
+        <AllMovies />
       </section>
     </div>
   );
--- challenge/one-cinema-app/src/app/(with-searchbar)/search/page.tsx
+++ challenge/one-cinema-app/src/app/(with-searchbar)/search/page.tsx
@@ -1,14 +1,29 @@
 import movies from "@/mock/movies.json"
 import MovieItem from "@/components/movie-item";
 import style from "./page.module.css"
+import { MovieData } from "@/types";
 
-export default function Page({
+export default async function Page({
     searchParams
 } : {
     searchParams: {
         q?: string
     }
 }) {
+    /**
+     * 현재 프로젝트에서는 검색 결과 데이터도 변하지 않을 것 같으므로 'force-cache' 설정.
+     * 'no-store'와 'force-cache' 비교 시 캐싱이 된 상태의 검색 속도가 약 20ms 정도 빨라진거 확인.
+     * 'no-store' 일 때는 계속 서버에 요청해서 시간이 일정함.
+     * */ 
+    
+    const res = await fetch(
+        `${process.env.NEXT_PUBLIC_API_SERVER_URL}/movie/search?q=${searchParams.q}`,
+        { cache: 'force-cache' }
+    )
+    if(!res.ok) return <div>오류가 발생하였습니다!!!</div>
+
+    const movies:MovieData[] = await res.json();
+
     return (
         <ul className={style.search_container}>
             {movies.map((movie) => (
