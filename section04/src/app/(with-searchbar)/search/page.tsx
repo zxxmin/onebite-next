5cc2309adc4d493f0711d77195e316f89f61f0f4
songjimin <ssong503503@gmail.com>
2024-10-07 23:32:46 +0900

    앱 라우터 데이터 페칭 + 데이터 캐시 + 리퀘스트 메모이제이션

---
+++ section04/src/app/(with-searchbar)/search/page.tsx
@@ -0,0 +1,23 @@
+import BookItem from "@/components/book-item";
+import { BookData } from "@/types";
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams: {
+    q?: string;
+  };
+}) {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/search?q=${searchParams.q}`)
+  if(!response.ok) return <div>오류가 발생하였습니다...</div>
+
+  const books : BookData[] = await response.json();
+  
+  return (
+    <div>
+      {books.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
+    </div>
+  );
+}
