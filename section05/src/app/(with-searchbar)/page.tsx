5b29327a4b69997a485680a4320e09e4c06efe44
songjimin <ssong503503@gmail.com>
2024-10-10 23:36:30 +0900

    풀 라우트 캐시1 + 풀 라우트 캐시2 + 풀 라우트 캐시3

---
+++ section05/src/app/(with-searchbar)/page.tsx
@@ -0,0 +1,54 @@
+import BookItem from "@/components/book-item";
+import style from "./page.module.css";
+import books from "@/mock/books.json";
+import { BookData } from "@/types";
+
+async function AllBooks () {
+  // cache 변경
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`,
+    {cache: 'force-cache'}
+  );
+
+  if(!response.ok) return <div>오류가 발생하였습니다...</div>
+
+  const allBooks : BookData[] = await response.json();
+
+  return <div>
+    {allBooks.map((book) => (
+      <BookItem key={book.id} {...book} />
+    ))}
+  </div>
+}
+
+async function RecoBooks () {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/random`,
+    {next: {revalidate: 3}}
+  );
+  if(!response.ok) return <div>오류가 발생하였습니다...</div>
+
+  const recoBooks : BookData[] = await response.json();
+
+  return <div>
+    {recoBooks.map((book) => (
+      <BookItem key={book.id} {...book} />
+    ))}
+  </div>
+}
+
+export default function Home() {
+  
+  return (
+    <div className={style.container}>
+      <section>
+        <h3>지금 추천하는 도서</h3>
+        <RecoBooks />
+      </section>
+      <section>
+        <h3>등록된 모든 도서</h3>
+        <AllBooks />
+      </section>
+    </div>
+  );
+}
